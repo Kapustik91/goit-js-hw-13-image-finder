b391b4b0329e23d71e7a97609f257d98ed0156fb
Yuliia Kovalenko <kus_queen@icloud.com>
2020-11-13 11:15:34 +0200

    done

---
+++ goit_js_hw_13_image_finder/src/index.js
@@ -0,0 +1,70 @@
+import './styles.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, error } from '@pnotify/core';
+
+import ImagesFinderApiService from './apiService';
+import photoCard from './templates/photoCard.hbs';
+import LoadMoreBtn from './load-more-btn';
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    articlesContainer: document.querySelector('.js-gallery'),
+    // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+const imagesFinderApiService = new ImagesFinderApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+    e.preventDefault();
+
+
+
+    clearArticlesContainer();
+    imagesFinderApiService.query = e.currentTarget.elements.query.value;
+
+    if (imagesFinderApiService.query === '') {
+        return alert('Please enter what you are looking for');
+    }
+    
+    loadMoreBtn.show();
+    imagesFinderApiService.resetPage();
+    imagesFinderApiService.fetchImages()
+        .then(appendArticlesMarkup)
+        .catch(error({
+            text: "Sorry, I can't find it:( Try again!",
+             delay: 3000
+
+        }));;
+}
+
+function onLoadMore() {
+    loadMoreBtn.disable();
+    imagesFinderApiService.fetchImages()
+        .then(appendArticlesMarkup);
+        
+    loadMoreBtn.enable();
+    
+    setTimeout(function () {
+        window.scrollTo(0, 1000)
+        window.scrollTo({
+      top: 1000,
+            behavior: 'smooth',
+    })
+        },0);
+
+}
+
+function appendArticlesMarkup(articles) {
+    refs.articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
+}
+
+function clearArticlesContainer() {
+    refs.articlesContainer.innerHTML = '';
+}
